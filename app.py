bf9176489f0620d5f61a3e9a7d0fe288b70eaa3a
jbryan3091 <jbryan3091@gmail.com>
2021-10-04 00:57:15 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from itertools import product
+from os import remove
+import re
+from flask import Flask, jsonify, request
+
+app = Flask (__name__)
+
+
+from products import products
+
+#Testing Route
+@app.route('/ping')
+def ping():
+    #return ('pong!')
+    return jsonify({"message":"pong!"})
+
+#Getting data Routes
+@app.route('/products')
+def getProducts():
+    return jsonify({"products": products, "message": "Product's List"})
+
+@app.route('/products/<string:product_name>')
+def getProduct(product_name):
+    productsFound = [product for product in products if product['name'] == product_name.lower()]
+    if(len(productsFound) > 0):
+        return jsonify({"product":productsFound[0]})
+    return jsonify({"message": "product not found"})
+
+#Creating data Route
+@app.route('/products', methods=['POST'])
+def addProduct():
+    new_product = {
+        "name": request.json['name'],
+        "price": request.json['price'],
+        "quantity": 10
+    }
+    #print(request.json)
+    products.append(new_product)
+    return jsonify({"message":"Product added Successfully", "products":products})
+
+#Updating data Route
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def editProduct(product_name):
+    productsFound = [product for product in products if product['name']== product_name]
+    if (len(productsFound) > 0):
+        productsFound[0]['name'] = request.json['name']
+        productsFound[0]['price'] = request.json['price']
+        productsFound[0]['quantity'] = request.json['quantity']
+        return jsonify({
+            'message': 'Product Updated',
+            'product': productsFound[0] 
+        })
+    return jsonify({"message": "Product not found"})
+
+#Deleting data Route
+@app.route('/products/<string:product_name>', methods=['DELETE'])
+def deleteProduct(product_name):
+    productsFound = [product for product in products if product['name']== product_name]
+    if len(productsFound) > 0:
+        products.remove(productsFound[0])
+        return jsonify({
+            "message": "Product Deleted",
+            "products": products
+        })
+    return jsonify({"message": "Product Not Found"})
+
+        
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
---
+++ products.py
@@ -0,0 +1,6 @@
+products = [
+
+    {"name": "Laptop", "price" : 850.00, "quantity": 4},
+    {"name": "mouse", "price" : 50.00, "quantity": 10},
+    {"name": "monitor", "price" : 400.00, "quantity": 3}
+]
